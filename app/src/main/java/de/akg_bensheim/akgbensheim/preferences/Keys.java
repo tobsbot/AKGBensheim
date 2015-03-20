2531828663174bf9fe1ce7c13d184c5ed4003767
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-03-20 18:55:00 +0100

    minor

---
+++ app/src/main/java/de/akg_bensheim/akgbensheim/preferences/Keys.java
@@ -0,0 +1,12 @@
+package de.akg_bensheim.akgbensheim.preferences;
+
+/**
+ * The keys to access preferences
+ */
+public class Keys {
+    public static final String ONLY_WIFI = "pref_key_only_wifi";
+    public static final String WEBSITE = "pref_key_website";
+    public static final String LICENCE = "pref_key_licence";
+    public static final String VERSION = "pref_key_version";
+    public static final String CONTACT_SUPPORT = "pref_key_contact_support";
+}
